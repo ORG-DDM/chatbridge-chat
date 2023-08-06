2bf45c7013c5a3b0263aa741bbf0fc74b7a3cd8c
jhyuk <akajhyuk@gmail.com>
2023-08-07 02:31:06 +0900

    init

---
+++ src/main/java/chatbridgechat/ChatbridgeChatApplication.java
@@ -0,0 +1,13 @@
+package chatbridgechat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatbridgeChatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatbridgeChatApplication.class, args);
+	}
+
+}
---
+++ src/test/java/chatbridgechat/ChatbridgeChatApplicationTests.java
@@ -0,0 +1,13 @@
+package chatbridgechat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatbridgeChatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
